2ec630ea0b6c76e46dd1ef4e65481f5c526dd318
KevSalcedo <kevsalcedom@gmail.com>
2023-04-25 17:29:39 -0500

    create TaskAdapter

---
+++ app/src/main/java/com/example/todoapp/TaskAdapter.kt
@@ -0,0 +1,19 @@
+package com.example.todoapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TaskAdapter(var tasks: List<Task>, private val onTaskSelected: (Int) -> Unit) : RecyclerView.Adapter<TaskViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_todo_task, parent, false)
+        return TaskViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        holder.render(tasks[position])
+        holder.itemView.setOnClickListener { onTaskSelected(position) }
+    }
+
+    override fun getItemCount() = tasks.size
+}
