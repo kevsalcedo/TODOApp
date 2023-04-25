746e157443647b5827bc0ace99f80ed5d657a875
KevSalcedo <kevsalcedom@gmail.com>
2023-04-25 17:29:55 -0500

    create TaskViewHolder

---
+++ app/src/main/java/com/example/todoapp/TaskViewHolder.kt
@@ -0,0 +1,40 @@
+package com.example.todoapp
+
+import android.content.res.ColorStateList
+import android.graphics.Paint
+import android.view.View
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+
+class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    private val cbTask: CheckBox = view.findViewById(R.id.cbTask)
+    private val tvTask: TextView = view.findViewById(R.id.tvTask)
+
+    fun render(task: Task) {
+
+        if (task.isSelected) {
+            tvTask.paintFlags = tvTask.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+        } else {
+            tvTask.paintFlags = tvTask.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
+        }
+
+        cbTask.isChecked = task.isSelected
+
+        val color = when (task.category) {
+            TaskCategory.Business -> R.color.todo_business_category
+            TaskCategory.Personal -> R.color.todo_personal_category
+            TaskCategory.Other -> R.color.todo_other_category
+        }
+
+        cbTask.buttonTintList = ColorStateList.valueOf(
+            ContextCompat.getColor(
+                cbTask.context, color
+            )
+        )
+
+        tvTask.text = task.name
+    }
+}
